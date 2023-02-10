652e802184293a5244c0b2e2fd1d5500b02df8b0
Sergio <sergiohervas18@gmail.com>
2023-02-10 23:48:18 +0100

    Capitulo 4 casi arreglado

--- meloudy_app/lib/main.dart
+++ meloudy_app/lib/main.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
-
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hola! Bienvenid@ a Meloudy'),);
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text("MELOUDY"),
+          ),
+          body: Column(
+            children: [
+              Text("Hola"),
+              Text("esto"),
+              Text("es"),
+              Text("una"),
+              Text("aplicación"),
+              Text("de"),
+              Text("flutter"),
+            ],
+          )),
+    );
   }
-
 }
